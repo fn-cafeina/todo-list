dba3e21cbc3a313e71700253d6c04db520a75215
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-06-12 21:30:45 -0600

    feat: add PubSub arch

---
+++ src/js/PubSub.js
@@ -0,0 +1,26 @@
+class PubSub {
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  }
+
+  unsubscribe(event, callback) {
+    if (!this.events[event]) return;
+    this.events[event] = this.events[event].filter((fn) => fn !== callback);
+  }
+
+  publish(event, data) {
+    if (!this.events[event]) return;
+    this.events[event].forEach((callback) => callback(data));
+  }
+}
+
+const pubSub = new PubSub();
+
+export default pubSub;
