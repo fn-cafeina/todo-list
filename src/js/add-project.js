70a0938028ec188772e3904d27d28b1c0dc207ce
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-06-12 21:31:39 -0600

    feat: add "add project function"

---
+++ src/js/add-project.js
@@ -0,0 +1,10 @@
+import projects from "./Projects";
+import Project from "./Project";
+import { errorMsg } from "./utils";
+
+export default function addProject(projectData) {
+  if (projectData.title) {
+    const newProject = new Project(projectData.title);
+    projects.addProject(newProject);
+  } else errorMsg("Please enter a valid project title");
+}
